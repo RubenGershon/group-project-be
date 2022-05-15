2bf7c9705453aab2806f7b85c3551688574785c0
Ruben Gershon <65229791+RubenGershon@users.noreply.github.com>
2022-05-15 23:15:48 +0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+
+mongoose.connect(process.env.MONGOOSE_CREDS);
+
+const app = new express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({ origin: ["http://localhost:3000"], credentials: true }));
+
+
+app.get("/", (req, res) => res.send(req.body));
+
+app.listen(process.env.PORT, () => {
+  console.log(`Group project app listening on port ${process.env.PORT}...`);
+});
